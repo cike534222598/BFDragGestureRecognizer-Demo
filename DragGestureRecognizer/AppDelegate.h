ccf62a6280a194abbba970cfed55d28b5be5a3f5
Shaco <cike534222598@qq.com>
2021-01-13 11:41:05 +0800

    修改BundleId

---
+++ DragGestureRecognizer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DragGestureRecognizer
+//
+//  Created by 菅帅博 on 2021/1/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DragGestureRecognizer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DragGestureRecognizer
+//
+//  Created by 菅帅博 on 2021/1/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
